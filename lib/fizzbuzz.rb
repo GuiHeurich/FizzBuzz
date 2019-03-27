7e5b76de147ad6cbdc7639f79fb899e51f90ec79
Oliver Cripps <ocripps24@users.noreply.github.com>
2019-03-27 12:26:42 +0000

    refactor and implement self methodology

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,11 +1,15 @@
-def fizzbuzz(number)
-  if number % 3 == 0 && number % 5 == 0
-    return 'fizzbuzz'
-  elsif number % 5 == 0
-    return 'buzz'
-  elsif number % 3 == 0
-    return 'fizz'
-  else
-    return number
-  end
-end
+# def fizzbuzz(number)
+#  if number % 3 == 0 && number % 5 == 0
+#    return 'fizzbuzz'
+#  elsif number % 5 == 0
+#    return 'buzz'
+#  elsif number % 3 == 0
+#    return 'fizz'
+#  else
+#    return number
+#  end
+# end
+
+def fizzbuzz
+  self % 15 == 0 ? "fizzbuzz" : self % 5 == 0 ? "buzz" : self % 3 == 0 ? "fizz" : self
+end 
