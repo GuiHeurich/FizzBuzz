bf581327987a7b965005f920aa82c86c035127f3
Oliver Cripps <ocripps24@users.noreply.github.com>
2019-03-27 12:37:11 +0000

    update testing methodology to work with self

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,16 +1,15 @@
 require 'fizzbuzz'
 describe 'fizzbuzz' do
-  number = (0..100)
   it 'returns "fizzbuzz" when passed 15' do
-    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+    expect(15.fizzbuzz).to eq 'fizzbuzz'
   end
   it 'returns "buzz" when passed 5' do
-    expect(fizzbuzz(5)).to eq 'buzz'
+    expect(5.fizzbuzz).to eq 'buzz'
   end
   it 'returns "fizz" when passed 3' do
-    expect(fizzbuzz(3)).to eq 'fizz'
+    expect(3.fizzbuzz).to eq 'fizz'
   end
   it 'returns the number if number is not divisible by 3 or 5' do
-    expect(fizzbuzz(number)).to eq number
+    expect(2.fizzbuzz).to eq 2
   end
 end
