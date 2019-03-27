5d64c4228f668a8c77eba9d23c7eade6078451e2
Oliver Cripps <ocripps24@users.noreply.github.com>
2019-03-27 12:36:04 +0000

    reflect original testing methodology

--- spec/fizzbuzz_alternate_spec.rb
+++ spec/fizzbuzz_alternate_spec.rb
@@ -1,16 +1,15 @@
-require 'fizzbuzz'
+require 'fizzbuzz_alternate'
 describe 'fizzbuzz' do
-  number = (0..100)
   it 'returns "fizzbuzz" when passed 15' do
-    expect((15).fizzbuzz).to eq 'fizzbuzz'
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
   end
   it 'returns "buzz" when passed 5' do
-    expect((5).fizzbuzz).to eq 'buzz'
+    expect(fizzbuzz(5)).to eq 'buzz'
   end
   it 'returns "fizz" when passed 3' do
-    expect((3).fizzbuzz).to eq 'fizz'
+    expect(fizzbuzz(3)).to eq 'fizz'
   end
   it 'returns the number if number is not divisible by 3 or 5' do
-    expect((number).fizzbuzz).to eq number
+    expect(fizzbuzz(2)).to eq 2
   end
 end
